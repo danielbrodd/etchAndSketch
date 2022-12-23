e7d3d1747b45236794ed815d9a9ddfe114501071
danielbrodd <daniel.brodd@gmail.com>
2022-12-23 07:31:09 +0100

    Add comments and change structeur of js

--- script.js
+++ script.js
@@ -6,13 +6,8 @@ const clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', reloadGrid)
 sizerBtn.addEventListener('click', changeSize);
 
-function changeSize(v) {
-    v = prompt();
-    if (v < 100){
-    setUpGrid(v)
-    }else setUpGrid(100);
-}
 
+//Creates the divs, adds css class, adds an event listner, and nests it in grid container
 function setUpGrid (size){
 clearGrid()
 grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -27,22 +22,33 @@ for (i = 0; i< (size*size); i++) {
 }
 }
 
+//changes the size of the grid by prompt and resets it
+function changeSize(v) {
+    v = prompt();
+    if (v < 100){
+    setUpGrid(v)
+    }else setUpGrid(100);
+}
 
+//colorchange function
 function colorChange(e) {
     e.target.style.backgroundColor = '#111';
 }
+
+//clears the grid container
 function clearGrid () {
     grid.innerHTML ='';
 }
+//function for clearing the grid and resetting it to 16x16
 function reloadGrid() {
     clearGrid();
     setUpGrid(16); 
 }
 
 
-
+//sets up the grid on pageload.
 window.onload = () => {
     setUpGrid(16);
 }
 
-//Didn't add 'extra credit features
+//Didn't add 'extra credit features will return to it later.
